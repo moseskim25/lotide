2a2ba12576d147ff9f3417049570b507b448051d
Moses Kim <moseskim4740@gmail.com>
2021-04-27 19:51:00 +0000

    Creates new array that filters out specified values

---
+++ without.js
@@ -0,0 +1,27 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  else console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+}
+
+const without = function(arr1, arr2) {
+  let filtered = arr1.filter(function(element) {
+    return arr2.indexOf(element) === -1;
+  });
+  return filtered;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
