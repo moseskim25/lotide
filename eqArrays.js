5c1f5b412a25c499ec12404cbafc0e822508b4c1
Moses Kim <moseskim4740@gmail.com>
2021-04-30 22:10:56 +0000

    Modified function using recursion so that it will interpolate and unlimited number of nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -5,11 +5,19 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
+
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!(eqArrays(arr1[i], arr2[i]))) return false;
+    }
+    else if (arr1[i] !== arr2[i]) return false;
   }
+
   return true;
 }
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
