470293ef7cb7fda024bfd9a345abe960c4848d8e
Moses Kim <moseskim4740@gmail.com>
2021-05-03 22:52:23 +0000

    Removed unnecessary codes since moving to mocha chai testing method

--- countOnly.js
+++ countOnly.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 let countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
@@ -10,22 +8,3 @@ let countOnly = function(allItems, itemsToCount) {
 }
 
 module.exports = countOnly;
-
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1);
-// assertEqual(result1["Karima"], undefined);
-// assertEqual(result1["Fang"], 2);
-// assertEqual(result1["Agouhanna"], undefined);
