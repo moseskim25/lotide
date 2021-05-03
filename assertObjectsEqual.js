b64fd32f02fad672189fc79ccab760306e488ee3
Moses Kim <moseskim4740@gmail.com>
2021-05-03 21:38:00 +0000

    Updated eqObjects function to include nested objects

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,3 @@
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
@@ -9,18 +8,23 @@ const eqArrays = function(arr1, arr2) {
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) return false;
-  for (const key1 in object1) {
-    if (Array.isArray(object1[key1])) return eqArrays(object1[key1], object2[key1]);
-    if (object1[key1] !== object2[key1]) return false;
-  }
-  
-  for (const key2 in object2) {
-    if (Array.isArray(object2[key2])) return eqArrays(object1[key2], object2[key2]);
-    if (object1[key2] !== object2[key2]) return false;
-  }
+
+  for (const key in object1) {
+
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) return false;
+
+    } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+      if (!(eqObjects(object1[key], object2[key]))) return false ;
+
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+
+  };
 
   return true;
-};
+}
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -28,10 +32,14 @@ const assertObjectsEqual = function(actual, expected) {
   else console.log(`🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
+const cd = { a: { z: 1 }, b: 2 };
+const dc = { a: { z: 1 }, b: 2 };
 const cd2 = { c: "1", d: ["2", 3, 4] };
+const cd3 = { c: "1", d: ["2", 3, 4] };
+const cd4 = { c: "1", d: ["2", 3, 4, 4] };
 
 // TEST CODE
 assertObjectsEqual(cd, dc);
 assertObjectsEqual(cd, cd2);
+assertObjectsEqual(cd2, cd3);
+assertObjectsEqual(cd2, cd4);
