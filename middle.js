f2b43d158ef4fcb8dc41203d7622078bf5a8d868
Moses Kim <moseskim4740@gmail.com>
2021-05-03 17:29:41 +0000

    Refactoring files by importing and exporting modules

--- middle.js
+++ middle.js
@@ -1,15 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-}
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) console.log(`✅ Assertion Passed: [${arr1}] === [${arr2}]`);
-  else console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-}
 
 const middle = function(arr) {
   let arrMid = (arr.length - 1) / 2;
@@ -21,6 +9,4 @@ const middle = function(arr) {
   return [arr[arrMid]];
 }
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
+module.exports = middle;
